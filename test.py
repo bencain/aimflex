461e6905fa07e073733358503152d819c1ec5032
Benjamin Cain <benjamin.m.cain@gmail.com>
2016-03-04 17:07:31 -0800

    minor edits

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ oned=np.linspace(-(ngrid-1)/2,(ngrid-1)/2,ngrid)
 x,y =np.meshgrid(oned,oned)
 
 
-flex=ai.AIMModel(logI=0.,alpha=6.,c1=0.,c2=0.,g1=-0.6,F1=-0.05,G1=0.15)
+flex=ai.AIMGaussian(logI=0.,alpha=6.,c1=0.,c2=0.,g1=-0.6,F1=-0.05,G1=0.15)
 
 Im=flex(x,y)
 
