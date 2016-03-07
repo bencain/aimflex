e8f705b56ab9adf4e1c1889feed6398d8fabc934
Benjamin Cain <benjamin.m.cain@gmail.com>
2016-03-07 15:21:34 -0800

    adding utils

--- aim.py
+++ aim.py
@@ -1,8 +1,25 @@
+
 import numpy as np
 import astropy.modeling as am
 
 
 class AIMGaussian(am.Fittable2DModel):
+	"""
+		This class implements an elliptical Gaussian source plane model lensed 
+		by shear and flexion.  Parameters included:
+			logI	- peak surface brightness
+			alpha	- size = sqrt(ab) = a*sqrt(E)
+			c1		- image plane x location for beta=0
+			c2		- image plane y location for beta=0
+			E1		- + polarized ellipticity
+			E2		- x polarized ellipticity
+			g1		- + polarized reduced shear
+			g2		- x polarized reduced shear
+			F1		- reduced 1-Flexion x
+			F2		- reduced 1-Flexion y
+			G1		- reduced 3-Flexion x
+			G2		- reduced 3-Flexion y
+	"""
 
 	logI  = am.Parameter(default=1.)
 	alpha = am.Parameter(default=1.)
@@ -43,86 +60,198 @@ class AIMGaussian(am.Fittable2DModel):
 		coo_c = np.conj(coo)
 		
 		beta = coo - g*coo_c - np.conj(F)*coo**2 \
-				- 2*F*coo*coo_c - G*coo_c**2 #- beta0 
+				- 2*F*coo*coo_c - G*coo_c**2 
 				
 		# Now for the model:
 		return gmodel(beta.real,beta.imag)
+
+
+def initial_parameters(image):
+	"""
+		A function for creating Gaussian model initial parameters from the data.
+	"""
+	
+	cts = calc_moments(image)
+	ctr = [calc_moments(image,xord=1),
+		   calc_moments(image,yord=1)]
+	Q11 = calc_moments(image,xord=2)/cts
+	Q22 = calc_moments(image,yord=2)/cts
+	Q12 = calc_moments(image,xord=1,yord=1)/cts
+	
+	m1=Q11-Q12**2/Q22
+	m2=Q22-Q12**2/Q11
+	m3=-Q12/(Q11*Q22-Q12**2)
+	
+	
+	
+
+#####################################################################################
+#################	UTILITIES	#####################################################
+#####################################################################################
+	
+def calc_moments(image,xord=0,yord=0):
+	"""
+		A function for calculating arbitrary xy image moments
+	"""
+	
+	if xord < 0:
+		xord=0
+	if yord < 0:
+		yord=0
+	dims=image.shape
+	
+	x,y =np.meshgrid(np.linspace(-(dims[0]-1)/2,(dims[0]-1)/2,dims[0]),
+					 np.linspace(-(dims[1]-1)/2,(dims[1]-1)/2,dims[1]))
+	
+	if (xord+yord) > 1:
+		cts = calc_moments(image,xord=0,yord=0)
+		ctr = [calc_moments(image,xord=1,yord=0)/cts,
+			   calc_moments(image,xord=0,yord=1)/cts]
+	else:
+		ctr=[0.,0.]
+	
+	return np.sum(image*np.power((x-ctr[0]),xord)*np.power((y-ctr[1]),yord))
+	
+	
+
+def convert_epars(epars,
+                  ae_to_pol=False, pol_to_mi=False,
+                  mi_to_ae=False,  pol_to_ae=False,
+                  mi_to_pol=False, ae_to_mi=False):
+	"""
+		This function converts between parametrizations of ellipse
+		parameters. We consider an elliptical profile given by
+			I(x,y)=I(r)
+			
+			r^2 = ((x-X_c)*cos(xi)+(y-Y_c)*sin(xi))^2/A^2 +
+				  ((y-Y_c)*cos(xi)-(x-X_c)*sin(xi))^2/(eps*A)^2
+
+		The center (X_c,Y_c) is not included in this function, just the 
+		three necessary shape parameters defined by one of the following 
+		parametrizations:
+
+		AE: Semimajor/minor axis geometric mean (AB)^0.5,
+			axis ratio EPS and position angle XI
+
+		POL: alpha=(AB)^0.5, "plus-mode" ellipticity EPLUS and
+			 "cross-mode" ellipticity ECROSS (evoking polarization)
+			 
+			 r^2=(1/alpha^2) * (1/sqrt(1-eplus^2-ecross^2) *
+				((1-eplus)*(x-X_c)^2 + (1+eplus)*(y-Y_c) -
+				2*ecross*(x-X_c)*(y-Y_c))
+
+		MI:  Inverse x^2 coefficient M1, inverse y^2 coefficient M2 and xy
+			 coefficient M3 from
+			 r^2 = ((x-X_c)^2/M1^2 + (y-Y_c)^2/M2 + 2*M3*(x-X_c)*(y-Y_c))
+	"""
+	outpars=np.copy(epars)
+	if not isinstance(epars,collections.Sequence):
+		print "aim.convert_epars requires a 3 element iterable input."
+		print " -> this looks like a scalar: ",epars
+		return outpars
+	
+	if len(epars) < 3:
+		print "aim.convert_epars requires a 3 element iterable input."
+		print " -> this looks like too few: ",epars
+		return outpars
+	elif len(epars) > 3:
+		print "aim.convert_epars requires a 3 element iterable input."
+		print " -> this looks like too many: ",epars
+		return outpars
+	
+	conds=[
+	if (not ae_to_pol) and pol_to_mi=False,
+                  mi_to_ae=False,  pol_to_ae=False,
+                  mi_to_pol=False, ae_to_mi=False
+
+	if ae_to_pol:
+	   A=epars[0]
+	   eps=epars[1]
+	   xi=epars[2]
+
+	   alpha=A*np.sqrt(eps)
+
+		# This is
+		# E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 )
+		# eplus= ((1d)-eps^2)*cos((2d)*xi)/((1d)+eps^2)
+		# ecross=((1d)-eps^2)*sin((2d)*xi)/((1d)+eps^2)
+
+		# E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 +2sqrt( Q11*Q22 - Q12^2 ) )
 		
-		
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-# class AIMImage:
-# 	"""
-# 		This class is for AIM fitting images.
-# 	"""
-# 	
-# 	def __init__(self,images):
-# 		"""
-# 			Return an AIMImage object based off the tuple of images: data, 
-# 			window, and psf.  Initial guess parameters are taken from the 
-# 			data image.
-# 		"""
-# 		
-# 		# Assumes (data, window, psf) format
-# 		# - > Saves to images attribute
-# 		# The data and window images are assumed to be the same size/shape
-# 		if isinstance(images,(list,tuple)):
-# 			if len(images) > 2 :
-# 				self.images=tuple(images)[0:3] 
-# 			elif len(images)>1 :
-# 				self.images=tuple(images)+(None,)
-# 			elif len(images)>0 :
-# 				self.images=tuple(images)+(None,None,)
-# 			else:
-# 				print "Bad input images for AIMImage"
-# 				self.images=(None,None,None)
-# 			
-# 		else:
-# 			self.images=(images,None,None,)
-# 					
-# 		self.pars_init=self.get_init_pars() 
-# 		self.pars_model=self.pars_init
-# 
-# 	
-# 	def get_init_pars(self):
-# 	
-# 		# XY grids
-# 		nx,ny=self.images[0].shape
-# 		linx=np.linspace(-(nx-1)/2.,(nx-1)/2.,nx)
-# 		liny=np.linspace(-(ny-1)/2.,(ny-1)/2.,ny)
-# 		x,y =np.meshgrid(linx,liny,indexing='ij')
-# 				
-# 		# Apply the window
-# 		if self.images[1] != None:
-# 			Iw=self.images[0]*self.images[1]
-# 		else: # or not
-# 			Iw=self.images[0]
-# 
-# 		#
-# 		flux=np.sum(Iw)
-# 		c1=np.sum(Iw*x)/flux
-# 		c2=np.sum(Iw*y)/flux
-# 		q11=np.sum(Iw*x**2)/flux - c1**2
-# 		q22=np.sum(Iw*y**2)/flux - c2**2
-# 		q12=np.sum(Iw*x*y)/flux - c1*c2
-# 		
-# 		# Size and ellipticity
-# 		alpha = np.sqrt(np.sqrt(q11*q22-q12**2))
-# 		e1=(q11-q22)/(q11+q22+2*np.sqrt(q11*q22-q12**2))
-# 		e2=2*q12/(q11+q22+2*np.sqrt(q11*q22-q12**2))
-# 		
-# 		print np.log10(flux),c1,c2,alpha,e1,e2
-# 		
-# 		return 0
-# 		
-# 		
+		eplus= (1.-eps)*np.cos(2.*xi)/(1.+eps)
+		ecross=(1.-eps)*np.sin(2.*xi)/(1.+eps)
+
+   		outpars[0:3]=alpha,eplus,ecross
+
+	else if pol_to_mi:
+	   alpha=epars[0]
+	   eplus=epars[1]
+	   ecross=epars[2]
+
+	   emag=sqrt(eplus^2+ecross^2)
+
+		# This is
+		# E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 )
+		# m1=alpha^2*sqrt(1d -emag^2)/(1d -eplus)
+		# m2=alpha^2*sqrt(1d -emag^2)/(1d +eplus)
+		# m3=-ecross/(alpha^2*sqrt(1d -emag^2))
+
+		# E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 +2sqrt( Q11*Q22 - Q12^2 ) )
+		m1=alpha**2*(1. - emag**2)/(1. + emag**2 - 2.*eplus)
+		m2=alpha**2*(1. - emag**2)/(1. + emag**2 + 2.*eplus)
+		m3=-2.*ecross/(alpha**2*(1. -emag**2))
+
+		outpars[ellipse_pars]=[m1,m2,m3]
+endif
+
+
+if keyword_set(pol_to_ae) then begin
+   alpha=pars[ellipse_pars[0]]
+   eplus=pars[ellipse_pars[1]]
+   ecross=pars[ellipse_pars[2]]
+   emag=sqrt(eplus^2+ecross^2)
+
+; This is
+;  E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 )
+;   eps=sqrt((1d -emag)/(1d +emag))
+;   A=alpha/sqrt(eps)
+;   xi=0.5d*atan(ecross,eplus)
+
+; This is
+;  E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 +2sqrt( Q11*Q22 - Q12^2 ) )
+   eps=(1d - emag)/(1d + emag)
+   A=alpha/sqrt(eps)
+   xi=0.5d*atan(ecross,eplus)
+
+   outpars[ellipse_pars]=[A,eps,xi]
+
+endif
+
+if keyword_set(mi_to_pol) then begin
+   m1=pars[ellipse_pars[0]]
+   m2=pars[ellipse_pars[1]]
+   m3=pars[ellipse_pars[2]]
+
+; This is
+;  E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 )
+;   alpha=(m1*m2/(1d - m1*m2*m3^2))^(0.25d)
+;   eplus=(m1-m2)/(m1+m2)
+;   ecross=-2d*m1*m2*m3/(m1+m2)
+
+; This is
+;  E = ( Q11 - Q22 + 2iQ12 )/( Q11 + Q22 +2sqrt( Q11*Q22 - Q12^2 ) )
+   alpha=(m1*m2/(1d - m1*m2*m3^2))^(0.25d)
+   eplus=(m1 - m2)/(m1 + m2 + 2d*sqrt(m1*m2 - (m1*m2*m3)^2))
+   ecross=-2d*m1*m2*m3/(m1 + m2 + 2d*sqrt(m1*m2 - (m1*m2*m3)^2))
+
+   outpars[ellipse_pars]=[alpha,eplus,ecross]
+
+endif
+
+if keyword_set(ae_to_mi) then $
+   outpars=convert_epars(convert_epars(pars,/ae_to_pol),/pol_to_mi)
+
+if keyword_set(mi_to_ae) then $
+   outpars=convert_epars(convert_epars(pars,/mi_to_pol),/pol_to_ae)
+
+return, outpars
--- test.py
+++ test.py
@@ -9,7 +9,7 @@ oned=np.linspace(-(ngrid-1)/2,(ngrid-1)/2,ngrid)
 x,y =np.meshgrid(oned,oned)
 
 
-flex=aim.AIMGaussian(logI=0.,alpha=6.,c1=0.,c2=0.,g1=-0.6,F1=-0.05,G1=0.15)
+flex=aim.AIMGaussian(logI=0.,alpha=6.,c1=10.,c2=0.) #,g1=-0.6,F1=-0.05,G1=0.15)
 
 Im=flex(x,y)
 
@@ -18,23 +18,25 @@ Im=flex(x,y)
 
 data=Im+np.random.randn(ngrid,ngrid)*0.1
 
-hdu1 = fits.PrimaryHDU(Im)
-hdu1.writeto('img.fits',clobber=True)
+# hdu1 = fits.PrimaryHDU(Im)
+# hdu1.writeto('img.fits',clobber=True)
+# 
+# hdu2 = fits.PrimaryHDU(data)
+# hdu2.writeto('dat.fits',clobber=True)
+# 
+# mod=aim.AIMGaussian(logI=0.,alpha=6.,c1=0.,c2=0.,g1=-0.6,F1=-0.05,G1=0.15)
+# 
+# fitter=fitting.LevMarLSQFitter()
+# 
+# fit = fitter(mod,x,y,data)
+# print fit
+# 
+# 
+# hdu3 = fits.PrimaryHDU(fit(x,y))
+# hdu3.writeto('fit.fits',clobber=True)
+# 
+# hdu4 = fits.PrimaryHDU(fit(x,y)-data)
+# hdu4.writeto('res.fits',clobber=True)
 
-hdu2 = fits.PrimaryHDU(data)
-hdu2.writeto('dat.fits',clobber=True)
 
-mod=aim.AIMGaussian(logI=0.,alpha=6.,c1=0.,c2=0.,g1=-0.6,F1=-0.05,G1=0.15)
-
-fitter=fitting.LevMarLSQFitter()
-
-fit = fitter(mod,x,y,data)
-print fit
-
-
-hdu3 = fits.PrimaryHDU(fit(x,y))
-hdu3.writeto('fit.fits',clobber=True)
-
-hdu4 = fits.PrimaryHDU(fit(x,y)-data)
-hdu4.writeto('res.fits',clobber=True)
-
+print aim.calc_moments(data,xord=1)/aim.calc_moments(data)
